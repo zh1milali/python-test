5b96fdfd816fdf52770d22dc6fd98c7e3615af40
zh1milali <rxkfjimy633@hotmail.com>
2025-12-12 14:51:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Last updated: 2025-12-12 14:45:29
+# Last updated: 2025-12-12 14:51:02
 
-def hello_world():
-    print("Hello, World!")
-    return "Success"
-
-if __name__ == "__main__":
-    hello_world()
+def hello_world():    print("Hello, World!")    return "Success"if __name__ == "__main__":    hello_world()
