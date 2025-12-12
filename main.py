253729dce1258bbb35c37689831ac1bbc203fb06
zh1milali <rxkfjimy633@hotmail.com>
2025-12-12 10:48:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-def hello_world():
-    print("Hello, World!")
-    return "Success"
-
-if __name__ == "__main__":
-    hello_world()
+def hello_world():    print("Hello, World!")    return "Success"if __name__ == "__main__":    hello_world()
+# Auto-generated comment
